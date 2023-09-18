66443963e03f251b01805a6ffb7e372b2530543f
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-09-18 10:27:02 +0200

    Add files via upload

---
+++ LINQ-Query-primjer/LINQ-Query-primjer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Query_primjer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string collection
+            List<string> stringList = new List<string>()
+            {
+                "C# Tutorials",
+                "VB.NET Tutorials",
+                "Learn C++",
+                "MVC Tutorials",
+                "Java"
+            };
+
+            //LINQ Query syntax
+            var result = from s in stringList
+                         where s.Contains("Tutorials")
+                         select s;
+            foreach (var str in result)
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();
+        }
+    }
+}
